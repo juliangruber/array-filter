7550a1f05b53618b453a1042e46d7f6cef7cb1ac
Julian Gruber <julian@juliangruber.com>
2013-05-22 21:39:42 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+/**
+ * Array#filter.
+ *
+ * @param {Array} arr
+ * @param {Function} fn
+ * @return {Array}
+ */
+
+module.exports = function (arr, fn) {
+  if (arr.filter) return arr.filter(fn);
+  var ret = [];
+  for (var i = 0; i < arr.length; i++) {
+    if (fn(arr[i], i, arr)) ret.push(arr[i]);
+  }
+  return ret;
+};
