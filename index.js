34211508c0b3015b28e5b01042334b36734a5120
Julian Gruber <julian@juliangruber.com>
2013-12-29 13:34:52 +0100

    add type checks

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 /**
  * Array#filter.
  *
@@ -9,6 +10,8 @@
 
 module.exports = function (arr, fn, self) {
   if (arr.filter) return arr.filter(fn);
+  if (void 0 === arr || null === arr) throw new TypeError;
+  if ('function' != typeof fn) throw new TypeError;
   var ret = [];
   for (var i = 0; i < arr.length; i++) {
     if (!hasOwn.call(arr, i)) continue;
