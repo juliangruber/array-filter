9c56c0f08548b89cf6e9de9c2b9466d15237d891
Julian Gruber <julian@juliangruber.com>
2013-12-29 12:18:21 +0100

    add thisArg. closes #4

--- index.js
+++ index.js
@@ -3,15 +3,16 @@
  *
  * @param {Array} arr
  * @param {Function} fn
+ * @param {Object=} self
  * @return {Array}
  */
 
-module.exports = function (arr, fn) {
+module.exports = function (arr, fn, self) {
   if (arr.filter) return arr.filter(fn);
   var ret = [];
   for (var i = 0; i < arr.length; i++) {
     if (!hasOwn.call(arr, i)) continue;
-    if (fn(arr[i], i, arr)) ret.push(arr[i]);
+    if (fn.call(self, arr[i], i, arr)) ret.push(arr[i]);
   }
   return ret;
 };
